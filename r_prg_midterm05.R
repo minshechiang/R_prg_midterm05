c0b73c3545db45f1ea4850dc9a1c52e5ad4a1280
minshechiang <minshechiang@gmail.com>
2017-01-28 21:50:49 +0800

    R programming midterm homework 05

---
+++ r_prg_midterm05.R
@@ -0,0 +1,9 @@
+load(url("https://storage.googleapis.com/r_rookies/straw_hat_df.RData"))
+library(magrittr)
+
+straw_hat_df$birth_date <- Sys.Date() %>%
+  format(., "%Y") %>%
+  as.numeric() %>%
+  `-` (straw_hat_df$age) %>%
+  paste(., straw_hat_df$birthday, sep="-") %>%
+  as.Date()
